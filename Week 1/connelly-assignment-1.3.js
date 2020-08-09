97afc13c897c214bc6843188fa1dec4d1f168929
mjconns <mjconns@gmail.com>
2020-08-09 11:14:03 -0500

    assignment 1.3

---
+++ Week 1/connelly-assignment-1.3.js
@@ -0,0 +1,28 @@
+/*
+============================================
+; Title:  Assignment 1.3
+; Author: Micah Connelly
+; Date:   8/9/2020
+; Description: Assignment 1.3 - modules
+;===========================================
+*/
+
+// Import header
+const header = require('../header-connelly');
+console.log(header.display('Micah', 'Connelly', 'Assignment 1.3'));
+
+
+// setting variable and requiring url
+var url = require('url');
+
+// url for example
+var parsedURL = url.parse('https://www.example.com/profile?name=micah');
+
+// finding protocol
+console.log(parsedURL.protocol);
+
+// finding host
+console.log(parsedURL.host);
+
+// finding query
+console.log(parsedURL.query);
