cfa19fcf2fd27657ffd1b68f28972d19d9680e84
mjconns <mjconns@gmail.com>
2020-09-07 11:20:02 -0500

    adding

--- Week 5/pug-template/connelly-assignment-5.3.js
+++ Week 5/pug-template/connelly-assignment-5.3.js
@@ -3,7 +3,7 @@
 ; Title: Assignment 5.3
 ; Author: Professor Krasso
 ; Modified: Micah Connelly
-; Date: 9/4/2020
+; Date: 9/7/2020
 ; Description: Assignment 5.3
 ===========================================
 */
@@ -11,3 +11,24 @@
 // Import header
 const header = require('../../header-connelly');
 console.log(header.display('Micah', 'Connelly', 'Assignment 5.3'));
+
+var express = require("express");
+var http = require("http");
+var pug = require("pug");
+var path = require("path");
+
+var app = express();
+
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "pug");
+
+app.get("/info", function (request, response) {
+    response.render("index", {
+        message: "Welcome to Pug Pups, USA"
+    });
+});
+
+// server start success
+http.createServer(app).listen(8080, function () {
+    console.log("Go to localhost:8080 in browser");
+});
